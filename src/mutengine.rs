6f3491934ef0cd639f794a68f458e9ebe32f12a7
root <suhailr7@ymail.com>
2019-03-30 21:54:16 +0000

    update

--- src/mutengine.rs
+++ src/mutengine.rs
@@ -1,1 +1,55 @@
+use crate::helpertools::{random};
+use crate::config::{ProgConfig, SeedConfig, Stat};
+use std::collections::VecDeque;
 
+
+pub fn mutate(seed_queue: &mut VecDeque<SeedConfig>) -> () {
+
+    ()
+
+}
+
+fn bit_flip(buf: Vec<u8>){
+
+}
+
+
+fn nibble_mod(buf: Vec<u8>,count: usize){
+
+}
+
+fn int_mod(buf: Vec<u8>){
+
+}
+
+fn ascii_mod(buf: Vec<u8>){
+
+}
+
+fn hot_values(buf: Vec<u8>){
+
+}
+
+fn arithmetic(buf: Vec<u8>,len: usize){
+
+}
+
+
+fn block_rm(buf: Vec<u8>){
+
+}
+
+fn block_insert(buf: Vec<u8>){
+
+}
+fn block_swap(buf: Vec<u8>){
+
+}
+
+fn block_shuffle(buf: Vec<u8>){
+
+}
+
+fn block_merge(buf: Vec<u8>){
+
+}
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,8 +1,8 @@
 use crate::config::{ProgConfig, SeedConfig, Stat};
 use crate::execengine::exec_fuzz;
 use crate::fuzzstat::FuzzerStatus;
-use rand;
-use rand::distributions::{Distribution, Uniform};
+use crate::mutengine::mutate;
+use crate::helpertools::{random};
 use std::collections::VecDeque;
 
 pub fn sched(
@@ -11,12 +11,24 @@ pub fn sched(
     fuzzer_status: &mut FuzzerStatus,
 ) {
     for _ in 0..10 {
-        let mut range = Uniform::from(0..seed_queue.len());
-        let mut rng = rand::thread_rng();
-        let mut random = range.sample(&mut rng);
-        exec_fuzz(&mut seed_queue[random], &progconfig);
-        fuzzer_status.update(seed_queue.len(), &seed_queue[random].exit_stat);
-        println!("\n\n      -- Fuzzer Status --\nStart Time     : {:?}\nTime Elapsed   : {:?}\nQueue Length   : {}\nCrash Count    : {}\nConfigs Tested : {}",&fuzzer_status.start_time.0,&fuzzer_status.time_elapsed,&fuzzer_status.queue_len,&fuzzer_status.crash_count,&fuzzer_status.conf_count);
-        //println!("Fuzzer_status\n {:?}\n\n",&fuzzer_status);
+        let rand = random(seed_queue.len());
+      
+        let new_seed = mutate(seed_queue);
+
+        exec_fuzz(&mut seed_queue[rand], &progconfig);
+        fuzzer_status.update(seed_queue.len(), &seed_queue[rand].exit_stat);
+    
+    println!("\n\n
+                 -- Fuzzer Status --\n
+                 Start Time     : {:?}\n
+                 Time Elapsed   : {:?}\n
+                 Queue Length   : {}\n
+                 Crash Count    : {}\n
+                 Configs Tested : {}"
+                 ,&fuzzer_status.start_time.0
+                 ,&fuzzer_status.time_elapsed
+                 ,&fuzzer_status.queue_len
+                 ,&fuzzer_status.crash_count
+                 ,&fuzzer_status.conf_count);
     }
 }
